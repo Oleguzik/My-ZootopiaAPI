135bae63e5c43a5c9fb4b41c3c0063b33a200166
oleguzik <guzik.oleg@gmail.com>
2025-09-18 12:59:20 +0200

    improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,44 +2,41 @@ import json
 
 def main():
     """
-    Read animals data from JSON file and template HTML, build HTML list items
-    per animal with <br/> separators, inject into template, and write animals.html.
+    Read animals data from JSON file, generate HTML with animal information.
     """
     try:
-        # Read the JSON file
         with open('animals_data.json', 'r') as file:
             animals_data = json.load(file)
         
-        # Read the HTML template
         with open('animals_template.html', 'r') as file:
             html_template = file.read()
         
-        # Generate animals output string as HTML list items
+        # Generate animals output string
         output = ""
         for animal in animals_data:
             characteristics = animal.get('characteristics', {})
             locations = animal.get('locations', [])
 
-            item_parts = []
             name = animal.get('name')
-            if name:
-                item_parts.append(f"Name: {name}<br/>\n")
-
             diet = characteristics.get('diet')
-            if diet:
-                item_parts.append(f"Diet: {diet}<br/>\n")
-
-            if locations:
-                item_parts.append(f"Location: {locations[0]}<br/>\n")
-
             animal_type = characteristics.get('type')
-            if animal_type:
-                item_parts.append(f"Type: {animal_type}<br/>\n")
-
-            # Only add a list item if we have at least one field
-            if item_parts:
+            
+            if name:
                 output += '<li class="cards__item">\n'
-                output += ''.join(item_parts)
+                output += f'  <div class="card__title">{name}</div>\n'
+                output += '  <p class="card__text">\n'
+                
+                # Add the details to card text
+                if diet:
+                    output += f'      <strong>Diet:</strong> {diet}<br/>\n'
+                
+                if locations:
+                    output += f'      <strong>Location:</strong> {locations[0]}<br/>\n'
+                
+                if animal_type:
+                    output += f'      <strong>Type:</strong> {animal_type}<br/>\n'
+                
+                output += '  </p>\n'
                 output += '</li>\n'
         
         # Replace the placeholder with the animals data
